5a3cac527ed8cc1f65823583cd469ebc1564c4b3
Azis202017 <azisa6980@gmail.com>
2024-01-24 05:17:39 +0700

    react router for pages spa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  About,
+  Error,
+  Landing,
+  Events,
+  Research,
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+} from './pages';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement : <Error/>,
+    element: <Landing />,
+  },
+  {
+    path: "/about-us",
+    element: <About />,
+  
+  },
+  {
+    path: "/research-publication",
+    element: <Research />,
+  },
+  {
+    path: "/events",
+    element: <Events />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
