f59a680d0d8285f09590179aaa246e62a8e063f3
Azis202017 <azisa6980@gmail.com>
2024-01-24 05:16:41 +0700

    Pages landing page, people pages, research page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Landing() {
+  return (
+    <>
+    <Link to={'/about-us'}>About us</Link>
+    </>
+  )
+}
+
+export default Landing
