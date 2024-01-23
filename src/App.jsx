7834763c70b52df0120aa375277edfe1ddc0d660
Azis202017 <azisa6980@gmail.com>
2024-01-24 04:20:50 +0700

    Boilerplate code vite'

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  function add() {
+    setCount(count + 1);
+  }
+  return (
+    <>
+ 
+    </>
+  );
+}
+
+export default App;
