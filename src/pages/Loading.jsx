41bea700a17e57374973607f54a12ca4c8f99b34
Azis202017 <azisa6980@gmail.com>
2024-01-25 16:22:05 +0700

    vh-100 to full screen

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -3,7 +3,7 @@ import Lottie from 'lottie-react';
 import LoadingLottie from '../assets/json/loading.json';
 function Loading() {
   return (
-    <div className='d-flex justify-content-center align-items-center'>
+    <div className='d-flex justify-content-center vh-100 align-items-center'>
       <Lottie animationData={LoadingLottie} loop={true} />
     </div>
   )
