8494314209d8be1f00b1e8e644ba02bfde5a8553
Azis202017 <azisa6980@gmail.com>
2024-01-25 09:53:35 +0700

    Loading

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Lottie from 'lottie-react';
+import LoadingLottie from '../assets/json/loading.json';
+function Loading() {
+  return (
+    <div className='d-flex justify-content-center align-items-center'>
+      <Lottie animationData={LoadingLottie} loop={true} />
+    </div>
+  )
+}
+
+export default Loading
