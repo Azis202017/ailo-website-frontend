87d70a497f5703f538857c50c9465d9372834778
Azis202017 <azisa6980@gmail.com>
2024-01-25 09:17:03 +0700

    Copy right footer

---
+++ src/components/footer/CopyRightFooter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../../assets/css/footer.css';
+function CopyRightFooter() {
+  return (
+    <footer className="second-footer">
+      <div className="d-flex justify-content-center">
+        <p className="second-footer-title">
+          © Copyright 2024. AI Center for Learning & Optimization Telkom
+          University
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default CopyRightFooter;
