5c387421d612e30b90e6e03f05cc54a4c9c283c6
Azis202017 <azisa6980@gmail.com>
2024-01-27 02:47:03 +0700

    Visit Us and Maps Contact component

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -6,28 +6,36 @@ import Instagram from '../assets/images/instagram.svg'
 import LinkedIn from '../assets/images/linkedin.svg'
 import Pesan from '../assets/images/pesan.svg'
 import Telepon from '../assets/images/telepon.svg'
+import VisitUs from '../components/contact_us/VisitUs';
+import MapsContact from '../components/contact_us/MapsContact';
 
 function ContactUs() {
   return (
     <>
       <GetInTouchContactUs />
-      <div className="row">
-        <div className="col-lg-3 mt-3">
-          <ContactCard icon={Telepon} name='+62 821-4514-3639' link="https://api.whatsapp.com/send/?phone=%2B6282116984458&text&type=phone_number&app_absent=0" alt="Telepon AILO" />
-        </div>
-        <div className="col-lg-3 mt-3">
-          <ContactCard name='ailo@telkomuniversity.ac.id' link="mailto:ailo@telkomuniversity.ac.id" icon={Pesan} />
+      <section id="contact-us-card">
+        <div className="row">
+          <div className="col-lg-3 mt-3">
+            <ContactCard icon={Telepon} name='+62 821-4514-3639' link="https://api.whatsapp.com/send/?phone=%2B6282116984458&text&type=phone_number&app_absent=0" alt="Telepon AILO" />
+          </div>
+          <div className="col-lg-3 mt-3">
+            <ContactCard name='ailo@telkomuniversity.ac.id' link="mailto:ailo@telkomuniversity.ac.id" icon={Pesan} />
+
+          </div>
+          <div className="col-lg-3 mt-3">
+            <ContactCard name='@ailo.telu' link="https://www.instagram.com/ailo.telu/?igsh=a3NlYW11aDhwNm02" icon={Instagram} />
+          </div>
+          <div className="col-lg-3 mt-3">
+            <ContactCard name='AILO RC' link="https://www.linkedin.com/company/ailo-rc/" icon={LinkedIn} />
+
+          </div>
 
         </div>
-        <div className="col-lg-3 mt-3">
-          <ContactCard name='@ailo.telu' link="https://www.instagram.com/ailo.telu/?igsh=a3NlYW11aDhwNm02" icon={Instagram} />
-        </div>
-        <div className="col-lg-3 mt-3">
-          <ContactCard name='AILO RC' link="https://www.linkedin.com/company/ailo-rc/" icon={LinkedIn} />
+      </section>
+      <VisitUs/>
+      <MapsContact/>
 
-        </div>
-        
-      </div>
+
 
     </>
   );
