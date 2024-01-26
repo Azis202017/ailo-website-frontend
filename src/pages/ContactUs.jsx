ddecff6c9feef49cd45854ae376766e2d2c87686
Azis202017 <azisa6980@gmail.com>
2024-01-27 01:48:59 +0700

    Contact Us

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import '../assets/css/contact_us.css';
+import GetInTouchContactUs from '../components/contact_us/GetInTouchContactUs';
+import ContactCard from '../components/contact_us/ContactCard';
+import Instagram from '../assets/images/instagram.svg'
+import LinkedIn from '../assets/images/linkedin.svg'
+import Pesan from '../assets/images/pesan.svg'
+import Telepon from '../assets/images/telepon.svg'
 
 function ContactUs() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <GetInTouchContactUs />
+      <div className="row">
+        <div className="col-lg-3 mt-3">
+          <ContactCard icon={Telepon} name='+62 821-4514-3639' link="https://api.whatsapp.com/send/?phone=%2B6282116984458&text&type=phone_number&app_absent=0" alt="Telepon AILO" />
+        </div>
+        <div className="col-lg-3 mt-3">
+          <ContactCard name='ailo@telkomuniversity.ac.id' link="mailto:ailo@telkomuniversity.ac.id" icon={Pesan} />
+
+        </div>
+        <div className="col-lg-3 mt-3">
+          <ContactCard name='@ailo.telu' link="https://www.instagram.com/ailo.telu/?igsh=a3NlYW11aDhwNm02" icon={Instagram} />
+        </div>
+        <div className="col-lg-3 mt-3">
+          <ContactCard name='AILO RC' link="https://www.linkedin.com/company/ailo-rc/" icon={LinkedIn} />
+
+        </div>
+        
+      </div>
+
+    </>
+  );
 }
 
 export default ContactUs
