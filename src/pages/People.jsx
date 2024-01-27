08a63df78d150deb78973a36ec255981b3dcc4a9
Azis202017 <azisa6980@gmail.com>
2024-01-28 05:45:55 +0700

    People

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Banner from '../components/about_us/Banner'
+import Maskot from '../assets/images/macot_peopel.png';
 function People() {
   return (
-    <div>People</div>
+    <div>
+      <Banner image={Maskot} title="Welcome to AILO Research Center" />
+    </div>
   )
 }
 
