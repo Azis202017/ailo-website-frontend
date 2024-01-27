d5efcddc396cb7b5a8e3cc42d2b23feb2e283ced
Azis202017 <azisa6980@gmail.com>
2024-01-27 14:01:09 +0700

    Added animate on scroll

--- src/components/contact_us/ContactCard.jsx
+++ src/components/contact_us/ContactCard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function ContactCard({ name, icon, link, alt}) {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
-    <a href={link} className="content-link mt-5">
-      <div className="contact-card">
+    <a href={link}  className="content-link mt-5">
+      <div className="contact-card" data-aos="zoom-in-left">
         <div className="d-flex">
           <img src={icon} alt={alt}  className='icon'/>
           <p className='title'>{name}</p>
