cb6d96c9ff6e634bf4296dc34575049771828766
Azis202017 <azisa6980@gmail.com>
2024-01-28 05:44:21 +0700

    Events banner

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Banner from '../components/about_us/Banner'
 function Events() {
   return (
-    <div>Events</div>
+    <div>
+           <Banner title="Events" />
+
+    </div>
   )
 }
 
