ef550571f27517ec695e35c13add7f577d5b5a29
Azis202017 <azisa6980@gmail.com>
2024-01-28 05:43:33 +0700

    maps contact component using animate on scroll

--- src/components/contact_us/MapsContact.jsx
+++ src/components/contact_us/MapsContact.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MapsContact() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   const redirectToWhatsApp = () => {
     const name = document.querySelector('.name-input').value;
     const email = document.querySelector('.email-input').value;
@@ -15,16 +20,20 @@ function MapsContact() {
   };
 
   return (
-    <section id="maps-contacts">
+    <section id="maps-contacts" data-aos="zoom-in-left">
       <div className="row">
         <div className="col-lg-6">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.3079099519423!2d107.628754!3d-6.9729531!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e9f150173951%3A0xeeda69399005c33!2sGedung%20D!5e0!3m2!1sen!2sid!4v1706295945355!5m2!1sen!2sid" className='maps' allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div className="col-lg-6">
           <h1 className="maps-contact-title">Get to Know Us Better</h1>
-          <input type="text" className="form-control email-text-placeholder email-input" placeholder='Email' />
-          <input type="text" className="form-control email-text-placeholder phone-input" placeholder='Phone' />
 
+          <div className="d-lg-flex">
+            <input type="text" className="form-control email-text-placeholder email-input" placeholder='Email' />
+            <input type="text" className="form-control email-text-placeholder phone-input w-100" placeholder='Phone' />
+
+
+          </div>
 
 
           <input type="text" className="form-control email-text-placeholder name-input" placeholder='Name' />
