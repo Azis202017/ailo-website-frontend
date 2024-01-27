78466b15c4cde6ec9bd9f7925d9f93f9c4f925ac
Azis202017 <azisa6980@gmail.com>
2024-01-28 05:43:43 +0700

    about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import '../assets/css/about_us.css'
+import Heading from '../components/about_us/heading';
+import Banner from '../components/about_us/Banner';
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className='w-100'>
+      <div className="d-none d-xl-flex justify-content-xl-end">
+        <div className="background-ungu">
+        </div>
+      </div>
+      <Heading />
+      <Banner title="AI Center for Learning & Optimization Telkom University" />
+     
+    </div>
+  );
 }
 
 export default About
