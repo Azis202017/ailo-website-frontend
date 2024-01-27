0e57de8b161f04325a55850482745dbc5592c2bb
Azis202017 <azisa6980@gmail.com>
2024-01-28 05:43:58 +0700

    Contact us

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import '../assets/css/contact_us.css';
 import GetInTouchContactUs from '../components/contact_us/GetInTouchContactUs';
 import ContactCard from '../components/contact_us/ContactCard';
@@ -10,13 +10,14 @@ import VisitUs from '../components/contact_us/VisitUs';
 import MapsContact from '../components/contact_us/MapsContact';
 
 function ContactUs() {
+
   return (
     <div className='contact-us'>
       <GetInTouchContactUs />
-      <section id="contact-us-card">
+      <section id="contact-us-card"  >
         <div className="row">
           <div className="col-lg-3 mt-3">
-            <ContactCard icon={Telepon} name='+62 821-4514-3639' link="https://api.whatsapp.com/send/?phone=%2B6282116984458&text&type=phone_number&app_absent=0" alt="Telepon AILO" />
+            <ContactCard  icon={Telepon} name='+62 821-4514-3639' link="https://api.whatsapp.com/send/?phone=%2B6282116984458&text&type=phone_number&app_absent=0" alt="Telepon AILO" />
           </div>
           <div className="col-lg-3 mt-3">
             <ContactCard name='ailo@telkomuniversity.ac.id' link="mailto:ailo@telkomuniversity.ac.id" icon={Pesan} />
@@ -25,15 +26,15 @@ function ContactUs() {
           <div className="col-lg-3 mt-3">
             <ContactCard name='@ailo.telu' link="https://www.instagram.com/ailo.telu/?igsh=a3NlYW11aDhwNm02" icon={Instagram} />
           </div>
-          <div className="col-lg-3 mt-3">
-            <ContactCard name='AILO RC' link="https://www.linkedin.com/company/ailo-rc/" icon={LinkedIn} />
+          <div className="col-lg-3 mt-3" >
+            <ContactCard  name='AILO RC' link="https://www.linkedin.com/company/ailo-rc/" icon={LinkedIn} />
 
           </div>
 
         </div>
       </section>
-      <VisitUs/>
-      <MapsContact/>
+      <VisitUs />
+      <MapsContact />
 
 
 
