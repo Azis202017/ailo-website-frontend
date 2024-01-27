2d32a1f07aa153b07a7969b8047ed4483049a803
Azis202017 <azisa6980@gmail.com>
2024-01-28 05:42:30 +0700

    Heading

---
+++ src/components/about_us/Heading.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <section className='about-us-heading'>
+
+      <div className="row">
+        <div className="col-lg-6">
+          <h1 className="about-independent-us-title">About Us</h1>
+          <p className="about-independent-us-subtitle">AI for Learning & Optimization (AILO) has become an Artificial Intelligence Center at Telkom University, Bandung, West Java since September 1st 2022. With 10 researchers lead by Prof. Dr. Suyanto with his vice Suryo Adhi Wibowo Ph.D, AILO has some research areas there are natural language processing (NLP), computer vision and image processing, prediction and optimization, swarm intelligence & evolutionary computation, and 4G AI. In order to become an excels and beneficial research center for society, AILO collaborate with faculties in Telkom University.</p>
+
+        </div>
+        <div className="col-lg-6">
+          <div className="foto">
+            <div className="front-foto"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Heading
