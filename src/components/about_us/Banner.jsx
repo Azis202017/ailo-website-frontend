fddae4c1ff51dc4f0a8230291ed7055139cb9c82
Azis202017 <azisa6980@gmail.com>
2024-01-28 08:41:00 +0700

    nonactive image if the image in small device

--- src/components/about_us/Banner.jsx
+++ src/components/about_us/Banner.jsx
@@ -4,9 +4,12 @@ function Banner({ title, image }) {
   return (
     < >
       <div className="banner-headline">
-        {image == null ? <span> <h1 className='d-flex justify-content-center align-items-center h-100 '>{title}</h1></span> : <div className='d-flex justify-content-center align-items-center h-100 w-100'>
+        {image == null ? <span> <h1 className='d-flex justify-content-center align-items-center h-100 '>{title}</h1></span> : <div className='d-flex  flex-lg-row justify-content-center align-items-center h-100 w-100'>
           <h1 >{title}</h1>
+          <div className="d-none d-lg-flex">
+
           <img src={image} alt="" className="banner-img" />
+          </div>
         </div>}
       </div>
     </>
