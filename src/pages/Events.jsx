1aad041e2623ee2c823652284227fa403e5dd202
Azis202017 <azisa6980@gmail.com>
2024-02-10 00:37:31 +0700

    Events

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Banner from '../components/about_us/Banner'
+import Loading from './Loading';
+import customAxios from '../service/axios_service';
+import '../assets/css/events.css'
 function Events() {
-  return (
-    <div>
-           <Banner title="Events" />
+  const [loading, setLoading] = useState(true);
+  const [categoryEvent, setCategoryEvent] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    </div>
+  const fetchData = async () => {
+    try {
+      let config = {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
+      };
+
+      const response = await customAxios.get('api/event', config);
+
+      setCategoryEvent(response.data.data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false); // Set loading to false in case of an error
+    }
+  };
+  return (
+    <>
+      {loading ? <Loading /> : <div>
+        <Banner title="Events" />
+
+        <section id="events-card">
+          <div className="row">
+
+            {categoryEvent.map(category => (
+
+              <div key={category.id} className="col-lg-6">
+                <a href={`/events/${category.id}`}>
+                  <div className="d-flex">
+                    <img
+                      src={category.foto_url}
+                      className="img-fluid"
+                      alt=""
+                    />
+                    <p className='events-card-title'>{category.title}</p>
+
+                  </div>
+                </a>
+
+
+              </div>
+
+            ))}
+          </div>
+
+
+
+
+
+        </section>
+
+      </div>}
+
+    </>
+
   )
 }
 
