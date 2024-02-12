d4272fa4e3ec30da1de36f7516a4159e07881be7
Azis202017 <azisa6980@gmail.com>
2024-02-12 16:55:48 +0700

    Detail research

---
+++ src/pages/DetailResearchArea.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function DetailResearchArea() {
+  const {id} = useParams();
+  return (
+    <div>DetailResearchArea</div>
+  
+    
+  )
+}
+
+export default DetailResearchArea
