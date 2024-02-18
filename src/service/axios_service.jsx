8605497056e39d1cce2af5523184f3d527dbdacf
Azis202017 <azisa6980@gmail.com>
2024-02-18 20:08:55 +0700

    refactor: change url service

--- src/service/axios_service.jsx
+++ src/service/axios_service.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 const customAxios = axios.create({
-  baseURL: 'https://7725-2001-448a-20e0-3f81-6d1b-2770-89d9-ecd2.ngrok-free.app'
+  baseURL: 'http://127.0.0.1:8000'
 });
 export default customAxios;
